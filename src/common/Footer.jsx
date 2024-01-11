5e0cc366e37890c8368d6474a139e7159de69bc6
Ramprakash <ramprakashsuthr2014@gmail.com>
2024-01-11 20:03:01 +0530

    Some Changes

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -4,13 +4,13 @@ export default function Footer() {
     return (
         <>
             <footer>
-                <div class="w-100 pt-5 pb-5"></div>
+                <div className ="w-100 pt-5 pb-5"></div>
                 <a href="" target="_blank">
-                    <div class="logo-originals-color mx-auto"></div>
-                </a>	<div class="w-100 p-3 text-secondary text-center small">
+                    <div className ="logo-originals-color mx-auto"></div>
+                </a>	<div className ="w-100 p-3 text-secondary text-center small">
                     <small>
-                        Part of the <a class="text-decoration-none fw-bold text-secondary" href="">AppSumo</a> family.
-                        © 2024 Sumo Group, Inc. All rights reserved. <a class="text-secondary" href="">Terms</a>. <a class="text-secondary" href="">Privacy</a>.</small>
+                        Part of the <a className ="text-decoration-none fw-bold text-secondary" href="">AppSumo</a> family.
+                        © 2024 Sumo Group, Inc. All rights reserved. <a className ="text-secondary" href="">Terms</a>. <a className ="text-secondary" href="">Privacy</a>.</small>
 
                 </div></footer>
         </>
--- src/pages/DatePolls.jsx
+++ src/pages/DatePolls.jsx
@@ -6,24 +6,24 @@ export default function DatePolls() {
     return (
         <>
             <Header />
-            <div class="w-100 d-block pb-4 pb-sm-5 mb-md-1"></div>
-            <div class="container">
+            <div className ="w-100 d-block pb-4 pb-sm-5 mb-md-1"></div>
+            <div className ="container">
 
-                <div class="row justify-content-center">
+                <div className ="row justify-content-center">
 
-                    <div class="col-12 col-lg-9 col-xl-8 col-xxl-7">
+                    <div className ="col-12 col-lg-9 col-xl-8 col-xxl-7">
 
-                        <div class="d-flex flex-wrap align-items-center justify-content-between w-100">
-                            <h1 class="h3 fw-bold d-inline me-3 mb-0 mb-4 mb-md-0">Date polls</h1>
+                        <div className ="d-flex flex-wrap align-items-center justify-content-between w-100">
+                            <h1 className ="h3 fw-bold d-inline me-3 mb-0 mb-4 mb-md-0">Date polls</h1>
 
-                            <div class="col-12 col-md d-flex align-items-center justify-content-between flex-wrap">
+                            <div className ="col-12 col-md d-flex align-items-center justify-content-between flex-wrap">
 
-                                <a href="" class="btn btn-outline-primary mb-3 mb-md-0 me-3">+ Create new date poll</a>
+                                <a href="" className ="btn btn-outline-primary mb-3 mb-md-0 me-3">+ Create new date poll</a>
 
-                                <div class="d-flex align-items-center mb-3 mb-md-0 col-8 col-sm-4">
-                                    <label class="me-2">View: </label>
-                                    <form class="w-100">
-                                        <select name="show" class="form-control">
+                                <div className ="d-flex align-items-center mb-3 mb-md-0 col-8 col-sm-4">
+                                    <label className ="me-2">View: </label>
+                                    <form className ="w-100">
+                                        <select name="show" className ="form-control">
                                             <option value="all">All polls</option>
                                             <option value="open">Open polls</option>
                                             <option value="finished">Finished polls</option>
@@ -38,11 +38,11 @@ export default function DatePolls() {
 
                 </div>
 
-                <div class="pb-4 mb-4"></div>
+                <div className ="pb-4 mb-4"></div>
 
-                <div class="row justify-content-center">
+                <div className ="row justify-content-center">
 
-                    <div class="col-12 col-lg-9 col-xl-8 col-xxl-7">
+                    <div className ="col-12 col-lg-9 col-xl-8 col-xxl-7">
                     </div>
                 </div>
             </div>
